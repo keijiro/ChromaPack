8799c0750af1359b976dced8753395d508c48154
tatsuhiko yamamura <tatsuhiko@unity3d.com>
2015-01-09 13:03:47 +0900

    add sprite samples

---
+++ Assets/Sprite and UI support/LoadSprite.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+
+public class LoadSprite : MonoBehaviour{
+
+	[SerializeField]
+	Texture2D texture;
+
+	void Start () 
+	{
+		var image = GetComponent<Image>();
+		
+		Sprite sprite = Sprite.Create(texture, new Rect(0,0, texture.width , texture.height), new Vector2(0.5f, 0.5f));
+		image.sprite = sprite;
+	}
+}
